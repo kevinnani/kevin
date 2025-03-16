bd739a192e149913802702c739754376de848b4c
kevinnani <125996138+kevinnani@users.noreply.github.com>
2025-03-16 11:58:05 +0530

    chnages for nav

--- src/redux/slices/contentSlices.js
+++ src/redux/slices/contentSlices.js
@@ -2,7 +2,7 @@ import {createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
 export const fetchContent = createAsyncThunk('content/fetchContent', async () => {
     try{
-        const response = await fetch('public/content.json');
+        const response = await fetch('https://kevinnani.github.io/kevin/content.json');
         if (!response.ok){
             throw new Error (` HTTP error status: ${response.status}`);
         }
